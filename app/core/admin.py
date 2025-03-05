597daa560fe58285dd25eccc5d4ba611cab7c3dd
Kaustubh Anirudha Vaidya <140918319+KaustubhVaidya404@users.noreply.github.com>
2025-03-05 11:41:21 +0530

    Super user and admin control done

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,26 @@
-# from django.contrib import admin
+# Django admin customization
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+from django.utils.translation import gettext_lazy as _
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'), {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,33 @@
+# Test for django admin modifications
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@example.com',
+            password = 'password'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'user@example.com',
+            password = 'pass'
+        )
+
+    def test_user_list(self):
+        url = reverse('admin:core_user_changelist')
+        result = self.client.get(url)
+
+        self.assertContains(result, self.user.email)
+
+    def test_edit_user_page(self):
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        result = self.client.get(url)
+
+        self.assertEqual(result.status_code, 200)
