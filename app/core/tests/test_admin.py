0b980e4bfce2ea559b38cc05da1b2fbb8b0694f3
Kaustubh Anirudha Vaidya <140918319+KaustubhVaidya404@users.noreply.github.com>
2025-03-05 11:55:36 +0530

    create user from admin console

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -31,3 +31,9 @@ class AdminSiteTests(TestCase):
         result = self.client.get(url)
 
         self.assertEqual(result.status_code, 200)
+
+    def test_create_user_page(self):
+        url = reverse("admin:core_user_add")
+        result = self.client.get(url)
+
+        self.assertEqual(result.status_code, 200)
